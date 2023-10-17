d2b9bd868d7a6dfba232886352a3625894a56c83
Rafael Botelho <botelhogama@gmail.com>
2023-10-17 14:01:02 -0300

    World gym project

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandasgui import show
 import matplotlib.pyplot as plt
 from utils import get_data, pre_process, plots
 
@@ -6,18 +7,5 @@ from utils import get_data, pre_process, plots
 data = pd.read_csv('C:\\Users\\Pichau\\PycharmProjects\\worldgym\\data\\data_raw.csv', index_col=0)
 
 pd.set_option('mode.chained_assignment', None)
-# data.drop(columns=['registerCancelDate', 'minPeriodStayMembership], inplace=True, errors= 'ignore')
-# data = data.iloc[:20]
-
-
-
-
-# dataset.loc[:, 'lastAccessDate'] = pd.to_datetime(dataset['lastAccessDate'], errors='coerce')
-# today = pd.Timestamp(datetime.now().date())
-# valid_timestamp_mask = dataset['lastAccessDate'].apply(lambda x: isinstance(x, pd.Timestamp)) # Mask for rows where lastAccessDate is a valid timestamp
-# dataset.loc[valid_timestamp_mask, 'date_since_last_access'] = (today - dataset.loc[valid_timestamp_mask, 'lastAccessDate'])
-# dataset['date_since_last_access'] = pd.to_timedelta(dataset['date_since_last_access']).dt.days
-# print(dataset['date_since_last_access'].describe())
-
-(dataset['date_since_last_access'][dataset['date_since_last_access'] < 600].hist(bins=30))
-plt.show()
+print(data.columns)
+show(data)
--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -2,12 +2,19 @@ import requests
 import pandas as pd
 import json
 import os
+import time
+import random
+
 
 class GetClientData:
-    def __init__(self, endpoint="/api/v1/members", take_number=25):
+    def __init__(self, endpoint="/api/v1/members", take_number=50, unidade='planaltina'):
         self.base_url = "https://evo-integracao.w12app.com.br/"
         self.user = "worldgym"
-        self.password = "44D6E01E-6DFF-4E0F-A787-73F0036496F1"
+        if unidade == "itapoa":
+            self.password = "44D6E01E-6DFF-4E0F-A787-73F0036496F1"
+        elif unidade == "planaltina":
+            self.password = "D1BC64B6-4EE7-4E16-974E-BD37F2F3DC2E"
+
         self.endpoint = endpoint
         self.params = {
             "take": take_number,
@@ -15,12 +22,18 @@ class GetClientData:
             "onlyPersonal": False,
             "showActivityData": True
         }
-        self.data = []
+        self.data = [ ]
 
     def make_api_request(self):
-        try:
-            while True:
-                response = requests.get(self.base_url + self.endpoint, auth=(self.user, self.password), params=self.params)
+        max_retries = 3
+        retries = 0
+
+        while retries < max_retries:
+            try:
+                response = requests.get(self.base_url + self.endpoint,
+                                        auth=(self.user, self.password),
+                                        params=self.params,
+                                        timeout=120)  # Increased timeout to 120 seconds
                 response.raise_for_status()
 
                 # Debugging Point: Print the status code
@@ -32,30 +45,44 @@ class GetClientData:
                 # Debugging Point: Print the number of records fetched so far
                 print(f"Total Records So Far: {len(self.data)}")
 
-                if len(current_data) < self.params["take"]:
+                if len(current_data) < self.params[ "take" ]:
                     break
-                self.params["skip"] += self.params["take"]
-                print(f"Retrieved {self.params['skip']} records so far")
+                self.params[ "skip" ] += self.params[ "take" ]
+                print(f"Retrieved {self.params[ 'skip' ]} records so far")
 
-        except requests.exceptions.RequestException as e:
-            print("Error:", e)
+                # Sleep for a random duration to avoid hitting rate limits
+                time.sleep(random.uniform(5, 10.0))
+
+            except requests.exceptions.ReadTimeout:
+                retries += 1
+                print(f"Request timed out. Attempt {retries} of {max_retries}.")
+                time.sleep(random.uniform(5, 10.0))
+
+            except requests.exceptions.RequestException as e:
+                print("Error:", e)
+                break
 
     def retrieve_data(self, use_cache=True):
-        cache_file_path = f"C:/Users/Pichau/PycharmProjects/worldgym/data/{self.endpoint.replace('/','_')}_cache.json"
+        cache_file_path = f"C:/Users/Pichau/PycharmProjects/worldgym/data/{self.endpoint.replace('/', '_')}_cache.json"
 
         if use_cache and os.path.exists(cache_file_path):
             with open(cache_file_path, "r") as file:
                 self.data = json.load(file)
+
+            # Update the skip parameter to skip already fetched records
+            self.params[ "skip" ] = len(self.data)
             print(f"Data loaded from cache for endpoint: {self.endpoint}")
-        else:
-            self.make_api_request()
-            print(f"Total records retrieved: {len(self.data)}")
-            os.makedirs(os.path.dirname(cache_file_path), exist_ok=True)
-            with open(cache_file_path, "w") as file:
-                json.dump(self.data, file)
-            print("Data cached.")
+            print(f"Starting data retrieval from record number: {self.params[ 'skip' ] + 1}")
+
+        # Fetch the data from the API, starting from the record after the cached data
+        self.make_api_request()
+        print(f"Total records retrieved (including cached): {len(self.data)}")
+
+        # Cache the updated data set
+        os.makedirs(os.path.dirname(cache_file_path), exist_ok=True)
+        with open(cache_file_path, "w") as file:
+            json.dump(self.data, file)
+        print("Data cached.")
 
         df = pd.DataFrame(self.data)
         return df
-
-
